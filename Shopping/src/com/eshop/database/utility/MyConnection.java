2a2bbaffb8fed273bb296669f3ce93920144e263
Rahul Madakatti <madakattirahul@gmail.com>
2015-09-21 09:39:30 +0530

    Login text box size  fixed correctly plus loading images

--- Shopping/src/com/eshop/database/utility/MyConnection.java
+++ Shopping/src/com/eshop/database/utility/MyConnection.java
@@ -15,7 +15,7 @@ public class MyConnection {
 	      {
 	        Class.forName("com.mysql.jdbc.Driver");  
 	       // con=DriverManager.getConnection("jdbc:mysql://localhost:3306/heenabou_shopprofile","heenabou_shop","rahulm@2015");
-	        con=DriverManager.getConnection("jdbc:mysql://localhost:3306/shopprofile","root","root	");
+	        con=DriverManager.getConnection("jdbc:mysql://localhost:3306/shopprofile","root","root");
 	      }
 	    }
 	    catch (Exception ex)
